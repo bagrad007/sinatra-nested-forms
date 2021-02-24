c9b00978caaab72fa5757f814ea4b573a3266849
Luke Gill <bagrad007@gmail.com>
2021-02-23 19:21:08 -0500

    commit tree 42de153720690805abc6f6f5f472667a399290b8

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
-require './environment'
+require "./environment"
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get "/" do
+      erb :root
+    end
 
+    get "/new" do
+      erb :"/pirates/new"
+    end
+
+    post "/pirates" do
+      pirate_name = params[:pirate][:name]
+      weight = params[:pirate][:weight]
+      height = params[:pirate][:height]
+      @pirate = Pirate.new(pirate_name, weight, height)
+
+      @ship1 = Ship.new(params[:pirate][:ships][0][:name], params[:pirate][:ships][0][:type], params[:pirate][:ships][0][:booty])
+      @ship2 = Ship.new(params[:pirate][:ships][1][:name], params[:pirate][:ships][1][:type], params[:pirate][:ships][1][:booty])
+
+      erb :"/pirates/show"
+    end
   end
 end
